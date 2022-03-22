f6204eea672162d47368716e0c109571e190b39e
ZynskiGit <korczynski.a@husky.neu.edu>
2022-03-22 19:49:32 -0400

    a6 React.js sp22

--- src/components/labs/conditional-output/conditional-output-inline.js
+++ src/components/labs/conditional-output/conditional-output-inline.js
@@ -3,8 +3,8 @@ const ConditionalOutputInline = () => {
     const loggedIn = false;
     return (
         <>
-            { loggedIn && <h2>Welcome Inline</h2>}      // if loggedIn is true, then this expression evaluates
-            {!loggedIn && <h2>Please login Inline</h2>}     // if loggedIn is false, then this expression evaluates
+            { loggedIn && <h2>Welcome Inline</h2>}
+            {!loggedIn && <h2>Please login Inline</h2>}
 
         </>
     );
