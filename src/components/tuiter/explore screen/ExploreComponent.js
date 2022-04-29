c007124ebfd0ec2518c665f21a2305f7aca441b2
ZynskiGit <korczynski.a@husky.neu.edu>
2022-04-29 15:15:42 -0400

    a8 REST sp22

--- src/components/tuiter/explore screen/ExploreComponent.js
+++ src/components/tuiter/explore screen/ExploreComponent.js
@@ -34,7 +34,7 @@ const ExploreComponent = () => {
                     </ul>
                 </div>
             </ul>
-            <div className="card" style="width:400px">
+            <div className="card" style={{width: '400px'}}>
                 <img className="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"></img>
                 <div className="card-body">
                     <h4 className="card-title">John Doe</h4>
